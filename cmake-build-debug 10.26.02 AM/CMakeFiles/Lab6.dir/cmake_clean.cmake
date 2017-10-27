1c78b2b8000700bc6288d809dd5d63efa0e004ae
COsborn2 <cameron0401@gmail.com>
2017-10-27 10:28:05 -0700

    Finalize

---
+++ cmake-build-debug 10.26.02 AM/CMakeFiles/Lab6.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab6.dir/linkedlist/linkedList.c.o"
+  "CMakeFiles/Lab6.dir/linkedlist/listUtils.c.o"
+  "CMakeFiles/Lab6.dir/pipes/pipes.c.o"
+  "CMakeFiles/Lab6.dir/process/process.c.o"
+  "CMakeFiles/Lab6.dir/tokenize/makeArgs.c.o"
+  "CMakeFiles/Lab6.dir/cscd340Lab6.c.o"
+  "CMakeFiles/Lab6.dir/utils/myUtils.c.o"
+  "CMakeFiles/Lab6.dir/utils/fileUtil.c.o"
+  "CMakeFiles/Lab6.dir/alias/alias.c.o"
+  "CMakeFiles/Lab6.dir/history/history.c.o"
+  "Lab6.pdb"
+  "Lab6"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Lab6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
